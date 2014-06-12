6bcf3e507f526401aebcfe75ead9d7719ee57853
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-06-12 21:29:08 +0530

    Add tags show and index with link to article and tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   resources :articles do 
     resources :comments
   end
-    
+  resources :tags
+      
   root to: "articles#index"  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
